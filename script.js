f38289a4c87d4f89934dc910f9f2faf7c0984685
Sanket Manmode <manmodesanket@gmail.com>
2020-04-26 18:31:55 +0530

    generating array and rendering it

---
+++ script.js
@@ -0,0 +1,37 @@
+let sortingMethod = "";
+
+let array = [];
+
+
+
+function between(min, max) {  
+    let random = Math.random();
+    let randomNumber = ((Math.floor(random * (max - min + 1)) + min));
+    return randomNumber;
+}
+
+function createArray() {
+    for(let i = 0; i < 20; i++) {
+        array.push(between(3, 20));
+    }
+    let arena = document.querySelector(".arena");
+    arena.innerHTML = '';
+    for(let i = 0; i < 20; i++) {
+        console.log(array[i]);
+        let element = document.createElement('div');
+        element.setAttribute('class', 'element');
+        element.style.height = (array[i] * 20) + "px"; 
+        arena.appendChild(element);
+    }
+    console.log("created");
+}
+
+
+let create = document.getElementById("create");
+
+create.addEventListener('click', createArray);
+
+
+
+
+
