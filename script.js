fd2492a78f00f3b804f03314685979a9aaffd364
Sanket Manmode <manmodesanket@gmail.com>
2020-04-28 00:12:12 +0530

    array create problem solved

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ let array = [];
 
 let sorting = 0;
 
+let canCreate = 1;
+
 function between(min, max) {
     let random = Math.random();
     let randomNumber = ((Math.floor(random * (max - min + 1)) + min));
@@ -32,21 +34,28 @@ async function createArray() {
 
 
 
-if (sorting == 0) {
+
     let create = document.getElementById("create");
     create.addEventListener('click', async function () {
-        await createArray();
-        sorting = 1;
+        if(canCreate === 1) {
+            canCreate = 0;
+            await createArray();
+            canCreate = 1;
+            sorting = 1;
+        }
+        
     });
-}
+
 
 
 let bubble = document.getElementById("bubble");
 bubble.addEventListener('click', async function () {
-    if (sorting == 1) {
+    if (sorting === 1) {
         sorting = 0;
+        canCreate = 0;
         await bubbleSort();
         sorting = 1;
+        canCreate = 1;
     }
 });
 
@@ -55,8 +64,10 @@ let insertion = document.getElementById("insertion");
 insertion.addEventListener('click', async function () {
     if (sorting === 1) {
         sorting = 0;
+        canCreate = 0;
         await insertionSort(array);
         sorting = 1;
+        canCreate = 1;
     }
 });
 
@@ -66,10 +77,12 @@ let merge = document.getElementById("merge");
 merge.addEventListener('click', async function () {
     if (sorting === 1) {
         sorting = 0;
+        canCreate = 0;
         let start = 0;
         let end = array.length - 1;
         await mergeSort(array, start, end);
         sorting = 1;
+        canCreate = 1;
     }
 });
 
@@ -78,11 +91,13 @@ let quick = document.getElementById("quick");
 quick.addEventListener('click', async function () {
     if (sorting === 1) {
         sorting = 0;
+        canCreate = 0;
         let l = 0;
         let h = array.length;
         array.push(1000000);
         await quickSort(array, l, h);
         sorting = 1;
+        canCreate = 1;
     }
 });
 
